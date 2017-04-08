db55aaf7075ac1ec9ed0d093226243c943d84648
unushessus <leopoldhess@gmail.com>
2017-04-08 18:27:09 +0200

    Exploratory Data Analysis Week 1 Assignment

---
+++ plot3.R
@@ -0,0 +1,24 @@
+## Read the data from February 1st and 2nd, 2007
+data <- read.csv("household_power_consumption.txt", sep = ";", header = F,
+                 col.names = c("Date", "Time", "Global_active_power",
+                               "Global_reactive_power", "Voltage",
+                               "Global_intensity", "Sub_metering_1",
+                               "Sub_metering_2", "Sub_metering_3"),
+                 skip = 66637, nrows = 2880)
+
+## Re-format dates and times in a new variable DateTime of class POSIXlt
+library(tidyr)
+data<-unite(data, DateTime, Date, Time, sep = "_", remove = TRUE)
+data$DateTime<-strptime(data$DateTime, format = "%d/%m/%Y_%H:%M:%S")
+
+
+## Create plot and save as png
+png("plot3.png")
+plot(data$DateTime, data$Sub_metering_1, type = "n", xlab="",
+     ylab = "Energy sub metering")
+lines(data$DateTime, data$Sub_metering_1)
+lines(data$DateTime, data$Sub_metering_2, col = "red")
+lines(data$DateTime, data$Sub_metering_3, col = "blue")
+legend("topright", lty = c(1,1), col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
